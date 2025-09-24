8d2206811cb447e1a726af3ab03f5c2ee6dd688e
SMILEZYd <bodedgadda@gmail.com>
2025-09-23 20:53:15 -0700

    Initial commit of Fabric Template
    Adds the fabric mods template to the repo

---
+++ src/client/java/com/smilezyd/musichud/MusicHUDClient.java
@@ -0,0 +1,10 @@
+package com.smilezyd.musichud;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MusicHUDClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
